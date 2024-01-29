55f129f3bdfc5f6edce1846d5a74067ec4fd8caf
AleksandrKonovaluk <aleksandr.konovaluk.awe@gmail.com>
2024-01-29 13:15:13 +0100

    Added login, getData endpoints

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
-using AuthWebApp.Infastructure;
+﻿using AuthWebApp.Infastructure;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authentication.OAuth;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
@@ -11,6 +12,28 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+	.AddJwtBearer(options =>
+	{
+		options.TokenValidationParameters = new TokenValidationParameters
+		{
+			// указывает, будет ли валидироваться издатель при валидации токена
+			ValidateIssuer = true,
+			// строка, представляющая издателя
+			ValidIssuer = AuthOptions.ISSUER,
+			// будет ли валидироваться потребитель токена
+			ValidateAudience = true,
+			// установка потребителя токена
+			ValidAudience = AuthOptions.AUDIENCE,
+			// будет ли валидироваться время существования
+			ValidateLifetime = true,
+			// установка ключа безопасности
+			IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
+			// валидация ключа безопасности
+			ValidateIssuerSigningKey = true,
+		};
+	});
 builder.Services.AddCors(c =>
 {
 	//c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin());
@@ -52,5 +75,23 @@ app.MapGet("/api/getToken", (string username, string password) =>
 })
 .WithName("GetToken")
 .WithOpenApi();
+
+app.Map("/login/{username}", (string username) =>
+{
+	var claims = new List<Claim> { new Claim(ClaimTypes.Name, username) };
+	// создаем JWT-токен
+	var jwt = new JwtSecurityToken(
+			issuer: AuthOptions.ISSUER,
+			audience: AuthOptions.AUDIENCE,
+			claims: claims,
+			expires: DateTime.UtcNow.Add(TimeSpan.FromMinutes(2)),
+			signingCredentials: new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
+
+	return new JwtSecurityTokenHandler().WriteToken(jwt);
+});
+
+app.MapGet("/api/getData", [Authorize] () => new { message = "dfsaf" })
+.WithName("GetData");
+
 
 app.Run();
